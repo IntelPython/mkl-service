f4fd934caee77914de6386890818c403917aa97c
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-05-23 09:53:31 -0500

    numpy and cython are required for setup, but six is required for install, for now

--- setup.py
+++ setup.py
@@ -99,7 +99,8 @@ def setup_package():
         platforms=["Windows", "Linux", "Mac OS-X"],
         test_suite='nose.collector',
         python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-        install_requires=['numpy', 'cython'],
+        setup_requires=['numpy', 'cython'],
+        install_requires=['six'],
         configuration=configuration,
     )
     setup(**metadata)
