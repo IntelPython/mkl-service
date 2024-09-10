fdcc87c4f728f4a262b344c4620707c0dff0aa58
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2024-09-10 08:15:32 -0500

    Do not use distutils build command, only install

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,4 +2,4 @@
 
 # make sure that compiler has been sourced, if necessary
 
-MKLROOT=$CONDA_PREFIX $PYTHON setup.py build --force install --old-and-unmanageable
+MKLROOT=$CONDA_PREFIX $PYTHON setup.py install
