1167a7c91f8221c92c7f2216de3de5344d558b8f
Anton Volkov <antonwolfy@gmail.com>
2025-06-26 11:16:25 -0500

    Add building wheel packages

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,5 +1,32 @@
-#!/bin/bash -x
+#!/bin/bash
+set -ex
 
-# make sure that compiler has been sourced, if necessary
+export MKLROOT=$CONDA_PREFIX
 
-MKLROOT=$CONDA_PREFIX $PYTHON -m pip install --no-build-isolation --no-deps .
+read -r GLIBC_MAJOR GLIBC_MINOR <<<"$(conda list '^sysroot_linux-64$' \
+    | tail -n 1 | awk '{print $2}' | grep -oP '\d+' | head -n 2 | tr '\n' ' ')"
+
+${PYTHON} setup.py clean --all
+
+# Make CMake verbose
+export VERBOSE=1
+
+# -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+${PYTHON} -m build -w -n -x
+
+${PYTHON} -m wheel tags --remove --build "$GIT_DESCRIBE_NUMBER" \
+    --platform-tag "manylinux_${GLIBC_MAJOR}_${GLIBC_MINOR}_x86_64" \
+    dist/mkl_service*.whl
+
+${PYTHON} -m pip install dist/mkl_service*.whl \
+    --no-build-isolation \
+    --no-deps \
+    --only-binary :all: \
+    --no-index \
+    --prefix "${PREFIX}" \
+    -vv
+
+# Copy wheel package
+if [[ -d "${WHEELS_OUTPUT_FOLDER}" ]]; then
+    cp dist/mkl_service*.whl "${WHEELS_OUTPUT_FOLDER[@]}"
+fi
